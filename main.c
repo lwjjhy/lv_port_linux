5b42206f80d6bc0d0718515839c8f9061d48b800
liaowj <lwjjhy@126.com>
2025-02-28 14:34:32 +0800

    修改cmake支持sdl2

--- main.c
+++ main.c
@@ -12,12 +12,9 @@
 #include "backends/interface.h"
 #endif
 
-uint16_t window_width;
-uint16_t window_height;
-bool fullscreen;
-bool maximize;
+uint16_t window_width = 1280;
+uint16_t window_height = 800;
 
-static void configure_simulator(int argc, char **argv);
 
 static const char *getenv_default(const char *name, const char *dflt)
 {
@@ -108,8 +105,17 @@ static void lv_linux_disp_init(void)
 #elif LV_USE_SDL
 static void lv_linux_disp_init(void)
 {
+    lv_display_t * disp = lv_sdl_window_create(window_width, window_height);
 
-    lv_sdl_window_create(window_width, window_height);
+    lv_group_t * g = lv_group_create();
+    lv_group_set_default(g);
+
+    lv_sdl_mouse_create();
+    lv_indev_t * mousewheel = lv_sdl_mousewheel_create();
+    lv_indev_set_group(mousewheel, lv_group_get_default());
+
+    lv_indev_t * keyboard = lv_sdl_keyboard_create();
+    lv_indev_set_group(keyboard, lv_group_get_default());
 
 }
 #elif LV_USE_WAYLAND
@@ -132,58 +138,185 @@ void lv_linux_run_loop(void)
 }
 #endif
 
-/*
- * Process command line arguments and environment
- * variables to configure the simulator
- */
-static void configure_simulator(int argc, char **argv)
+
+#if LV_USE_ARC && LV_BUILD_EXAMPLES
+
+static void value_changed_event_cb(lv_event_t * e);
+
+void lv_example_arc_1(void)
 {
+    lv_obj_t * label = lv_label_create(lv_screen_active());
 
-    int opt = 0;
-    bool err = false;
+    /*Create an Arc*/
+    lv_obj_t * arc = lv_arc_create(lv_screen_active());
+    lv_obj_set_size(arc, 150, 150);
+    lv_arc_set_rotation(arc, 135);
+    lv_arc_set_bg_angles(arc, 0, 270);
+    lv_arc_set_value(arc, 10);
+    lv_obj_center(arc);
+    lv_obj_add_event_cb(arc, value_changed_event_cb, LV_EVENT_VALUE_CHANGED, label);
 
-    /* Default values */
-    fullscreen = maximize = false;
-    window_width = atoi(getenv("LV_SIM_WINDOW_WIDTH") ? : "800");
-    window_height = atoi(getenv("LV_SIM_WINDOW_HEIGHT") ? : "480");
+    /*Manually update the label for the first time*/
+    lv_obj_send_event(arc, LV_EVENT_VALUE_CHANGED, NULL);
+}
 
-    /* Parse the command-line options. */
-    while ((opt = getopt (argc, argv, "fmw:h:")) != -1) {
-        switch (opt) {
-        case 'f':
-            fullscreen = true;
-            if (LV_USE_WAYLAND == 0) {
-                fprintf(stderr, "The SDL driver doesn't support fullscreen mode on start\n");
-                exit(1);
-            }
-            break;
-        case 'm':
-            maximize = true;
-            if (LV_USE_WAYLAND == 0) {
-                fprintf(stderr, "The SDL driver doesn't support maximized mode on start\n");
-                exit(1);
-            }
-            break;
-        case 'w':
-            window_width = atoi(optarg);
-            break;
-        case 'h':
-            window_height = atoi(optarg);
-            break;
-        case ':':
-            fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-            exit(1);
-        case '?':
-            fprintf (stderr, "Unknown option -%c.\n", optopt);
-            exit(1);
+static void value_changed_event_cb(lv_event_t * e)
+{
+    lv_obj_t * arc = lv_event_get_target_obj(e);
+    lv_obj_t * label = (lv_obj_t *)lv_event_get_user_data(e);
+
+    lv_label_set_text_fmt(label, "%" LV_PRId32 "%%", lv_arc_get_value(arc));
+
+    /*Rotate the label to the current position of the arc*/
+    lv_arc_rotate_obj_to_angle(arc, label, 25);
+}
+
+static void set_angle(void * obj, int32_t v)
+{
+    lv_arc_set_value((lv_obj_t *)obj, v);
+}
+
+/**
+ * Create an arc which acts as a loader.
+ */
+void lv_example_arc_2(void)
+{
+    /*Create an Arc*/
+    lv_obj_t * arc = lv_arc_create(lv_screen_active());
+    lv_obj_set_x(arc, 100);
+    lv_obj_set_y(arc, 100);
+
+    lv_arc_set_rotation(arc, 270);
+    lv_arc_set_bg_angles(arc, 0, 360);
+    lv_obj_remove_style(arc, NULL, LV_PART_KNOB);   /*Be sure the knob is not displayed*/
+    lv_obj_remove_flag(arc, LV_OBJ_FLAG_CLICKABLE);  /*To not allow adjusting by click*/
+    // lv_obj_center(arc);
+
+    lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_var(&a, arc);
+    lv_anim_set_exec_cb(&a, set_angle);
+    lv_anim_set_duration(&a, 1000);
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);    /*Just for the demo*/
+    lv_anim_set_repeat_delay(&a, 500);
+    lv_anim_set_values(&a, 0, 100);
+    lv_anim_start(&a);
+
+}
+#endif
+
+void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t *btn = lv_event_get_current_target(e);
+    switch (code) {
+    case LV_EVENT_CLICKED: {
+        printf("btn pressed!!!\r\n");
+        static int led_status = 0;
+        led_status++;
+        lv_obj_t *lab = lv_obj_get_child(btn, 0);
+        if(led_status%2) {
+            lv_obj_set_style_bg_color(btn, lv_color_hex(0xff0000), 0);
+            lv_label_set_text(lab, "LED OFF");
+        } else {
+            lv_obj_set_style_bg_color(btn, lv_color_hex(0x2196f3), 0);
+            lv_label_set_text(lab, "LED ON");
         }
+        /* code */
+        break;
     }
+    default:
+        break;
+    }
+    
+}
+
+void lv_example_btn(void)
+{
+    static lv_style_t style = {0};
+    lv_style_init(&style);
+    
+    /*Set a background color and a radius*/
+    lv_style_set_radius(&style, 15);
+    lv_style_set_bg_opa(&style, LV_OPA_COVER);
+    lv_style_set_bg_color(&style, lv_color_hex(0x2196f3));
+
+    /*Add border to the bottom+right*/
+    lv_style_set_border_color(&style, lv_color_hex(0x00ff00));
+    lv_style_set_border_width(&style, 5);
+    lv_style_set_border_opa(&style, LV_OPA_50);
+    lv_style_set_border_side(&style, LV_BORDER_SIDE_BOTTOM | LV_BORDER_SIDE_RIGHT);
+
+    /*Add a shadow*/
+    lv_style_set_shadow_width(&style, 55);
+    lv_style_set_shadow_color(&style, lv_palette_main(LV_PALETTE_BLUE));
+    lv_style_set_shadow_opa(&style, LV_OPA_100);
+    // rotation
+    lv_style_set_transform_rotation(&style, 120);
+    /*Create an Arc*/
+    lv_obj_t * btn = lv_btn_create(lv_screen_active());
+    lv_obj_add_event(btn, btn_event_cb, LV_EVENT_ALL, NULL);
+    lv_obj_set_x(btn, 100);
+    lv_obj_set_y(btn, 100);
+    lv_obj_set_size(btn, 180, 160);
+    lv_obj_add_style(btn, &style, 0);
+    
+
+    //label
+    lv_obj_t *lab = lv_label_create(btn);
+    lv_label_set_text(lab, "LED ON");
+    lv_obj_center(lab);
+
+    // /*Create an object with the new style*/
+    // lv_obj_t * image = lv_image_create(btn);
+    // LV_IMAGE_DECLARE(img_cogwheel_argb);
+    // lv_image_set_src(image, &img_cogwheel_argb);
+    // // lv_obj_move_to_index(image, -1);
+    // lv_obj_move_to_index(btn, -1);
+
+
+}
+
+void lv_example_style_6(void)
+{
+    static lv_style_t style;
+    lv_style_init(&style);
+
+    /*Set a background color and a radius*/
+    lv_style_set_radius(&style, 5);
+    lv_style_set_bg_opa(&style, LV_OPA_COVER);
+    lv_style_set_bg_color(&style, lv_palette_lighten(LV_PALETTE_GREY, 3));
+    lv_style_set_border_width(&style, 2);
+    lv_style_set_border_color(&style, lv_palette_main(LV_PALETTE_BLUE));
+
+    lv_style_set_image_recolor(&style, lv_palette_main(LV_PALETTE_BLUE));
+    lv_style_set_image_recolor_opa(&style, LV_OPA_50);
+    lv_style_set_transform_rotation(&style, 300);
+    
+    /*Create an object with the new style*/
+    lv_obj_t * obj = lv_image_create(lv_screen_active());
+    lv_obj_add_style(obj, &style, 0);
+
+    LV_IMAGE_DECLARE(img_cogwheel_argb);
+    lv_image_set_src(obj, &img_cogwheel_argb);
+
+    lv_obj_center(obj);
+}
+
+void xdev_demo(void)
+{
+    // #include "../../lv_examples.h"
+
+    // lv_example_arc_1();
+    // lv_example_arc_2();
+    lv_example_btn();
+    lv_example_style_6();
 }
 
 int main(int argc, char **argv)
 {
 
-    configure_simulator(argc, argv);
+    printf("main!!!\r\n");
 
     /* Initialize LVGL. */
     lv_init();
@@ -192,8 +325,9 @@ int main(int argc, char **argv)
     lv_linux_disp_init();
 
     /*Create a Demo*/
-    lv_demo_widgets();
-    lv_demo_widgets_start_slideshow();
+    // lv_demo_widgets();
+    // lv_demo_widgets_start_slideshow();
+    xdev_demo();
 
     lv_linux_run_loop();
 
